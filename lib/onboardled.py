afe6776d438199874fc3710fa76c5684203905e9
ip <ianperry99@gmail.com>
2018-06-04 04:53:43 +0000

    update

--- lib/onboardled.py
+++ lib/onboardled.py
@@ -9,7 +9,7 @@ def flash():
     os.system('echo heartbeat > /sys/class/leds/led0/trigger')
 
 def off():
-    os.system('echo 0 > /sys/class/leds/led0/brightness')
+    os.system('echo 1 > /sys/class/leds/led0/brightness')
 
 def on():
-    os.system('echo 1 > /sys/class/leds/led0/brightness')
+    os.system('echo 0 > /sys/class/leds/led0/brightness')
